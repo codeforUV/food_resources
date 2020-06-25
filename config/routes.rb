8e2ce13f57e774b828a75861fdd8e4a8195d0c35
Tom Chen <chen.thomas.c@gmail.com>
2020-06-25 19:29:34 -0400

    Make food_access_points#index the root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'food_access_points/index'
+  root 'food_access_points#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
