644036ad15030295abcb58c4c3f9f6e9c158bb9c
Bryan Hooper <bthooper@gmail.com>
2020-10-26 12:22:50 -0400

    added javascript to toggle menu.

--- app/javascript/packs/header.js
+++ app/javascript/packs/header.js
@@ -1,56 +1,11 @@
 document.addEventListener("turbolinks:load", function () {
-  // Handle menu trigger menu
-  document.getElementById("triggerMenu").addEventListener("click", triggerMenu);
+  document.getElementById("mobile-button").addEventListener("click", (e) => {
+    console.log("clicked");
+    let mM = document.getElementById("mobile-menu");
+    if (mM.style.display == "none") {
+      mM.style.display = "block";
+    } else {
+      mM.style.display = "none";
+    }
+  });
 });
-
-/**
- * Function that handles menu trigger
- */
-function triggerMenu() {
-  let links = document.getElementById("links");
-  let classList = links.classList;
-  let svg = document.getElementById("menuIcon");
-
-  // Menu is hidden
-  if (classList.contains("hidden")) {
-    // Show the menu
-    links.classList = replaceClass("hidden", "block", classList);
-
-    let closeSVG =
-      "M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z";
-    // Show close menu icon
-    svg.setAttribute("d", closeSVG);
-  } else {
-    // Hide the menu
-    links.classList = replaceClass("block", "hidden", classList);
-    let openSVG =
-      "M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z";
-    // Show open menu icon
-    svg.setAttribute("d", openSVG);
-  }
-  return true;
-};
-
-/**
- * Function that replaces a "oldClass" by a "newClass" in a "classList"
- * @param {*} oldClass 
- * @param {*} newClass 
- * @param {*} classList 
- */
-function replaceClass(oldClass, newClass, classList) {
-  classList.remove(oldClass);
-  classList.add(newClass);
-  return classList;
-}
-
-document.addEventListener('turbolins:load', function() {
-
-  document.getElementById('mobile-menu').addEventListener('click', toggleMenu);
-});
-
-funciton toggleMenu() {
-  if this.classList.contains('hidden') {
-    this.className.replaceClass('hidden', 'block', this.className)
-  }
-}
-
