2b3ef8d04eb52b55ade088368192726f97902f7a
Bryan Hooper <bthooper@gmail.com>
2020-10-26 12:24:34 -0400

    oops! removed debugging console.log

--- app/javascript/packs/header.js
+++ app/javascript/packs/header.js
@@ -1,6 +1,5 @@
 document.addEventListener("turbolinks:load", function () {
   document.getElementById("mobile-button").addEventListener("click", (e) => {
-    console.log("clicked");
     let mM = document.getElementById("mobile-menu");
     if (mM.style.display == "none") {
       mM.style.display = "block";
