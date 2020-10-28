7196ee9aa480736854a2acb733581cca7c4c91b3
Bryan Hooper <bthooper@gmail.com>
2020-10-28 09:50:58 -0400

    improved variable names

--- app/javascript/packs/header.js
+++ app/javascript/packs/header.js
@@ -2,23 +2,23 @@
 
 document.addEventListener("turbolinks:load", function () {
   document.getElementById("mobile-button").addEventListener("click", () => {
-    let mM = document.getElementById("mobile-menu");
-    let mIO = document.getElementById("menu-icon-open");
-    let mIC = document.getElementById("menu-icon-closed");
-    if (mM.classList.contains("hidden")) {
-      mM.classList.remove("hidden");
-      mM.classList.add("block");
-      mIO.classList.add("block");
-      mIO.classList.remove("hidden");
-      mIC.classList.add("hidden");
-      mIC.classList.remove("block");
+    let mobileMenu = document.getElementById("mobile-menu");
+    let menuIconOpen = document.getElementById("menu-icon-open");
+    let mIconClosed = document.getElementById("menu-icon-closed");
+    if (mobileMenu.classList.contains("hidden")) {
+      mobileMenu.classList.remove("hidden");
+      mobileMenu.classList.add("block");
+      menuIconOpen.classList.add("block");
+      menuIconOpen.classList.remove("hidden");
+      menuIconClosed.classList.add("hidden");
+      menuIconClosed.classList.remove("block");
     } else {
-      mM.classList.add("hidden");
-      mM.classList.remove("block");
-      mIO.classList.add("hidden");
-      mIO.classList.remove("block");
-      mIC.classList.add("block");
-      mIC.classList.remove("hidden");
+      mobileMenu.classList.add("hidden");
+      mobileMenu.classList.remove("block");
+      menuIconOpen.classList.add("hidden");
+      menuIconOpen.classList.remove("block");
+      menuIconClosed.classList.add("block");
+      menuIconClosed.classList.remove("hidden");
     }
   });
 });
