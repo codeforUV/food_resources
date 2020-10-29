e002277017aa1343196207414acc17e7c6c073e8
Bryan Hooper <bthooper@gmail.com>
2020-10-29 08:54:39 -0400

    removed local host configuration used for local development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.hosts << "zech.local"
 end
