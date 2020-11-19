9d0bb10573c3f2bc75dd72e823cc9e6caa9c307f
Bryan Hooper <bthooper@gmail.com>
2020-11-19 12:46:32 -0500

    added forms for food_access_points edit/add and updated routes

--- app/controllers/food_access_points_controller.rb
+++ app/controllers/food_access_points_controller.rb
@@ -1,4 +1,7 @@
 class FoodAccessPointsController < ApplicationController
+
+  before_action :authenticate_user!, :except => [:index, :show]
+
   def index
     @food_access_points = FoodAccessPoint.all
   end
@@ -6,4 +9,32 @@ class FoodAccessPointsController < ApplicationController
   def show
     @food_access_point = FoodAccessPoint.find(params[:id])
   end
+  
+  def new
+    @food_access_point = FoodAccessPoint.new
+  end
+
+  def create
+    Rails.logger.debug params.inspect
+    @food_access_point = current_user.food_access_points.build(food_access_point_params(:name, :address, :town, :state, :zip, :phone_number, :website))
+  end
+
+  def edit
+    @food_access_point = FoodAccessPoint.find(params[:id])
+  end
+
+  def update
+
+  end
+
+  def destroy
+  end
+
+  private 
+
+  def food_access_point_params(*args) 
+    params.require(:food_access_point).permit(*args)
+  end
+
+
 end
