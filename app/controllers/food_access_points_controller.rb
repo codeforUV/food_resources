de25bf5e6a81526e1bb13716bf9ab6f40bca9505
Bryan Hooper <bthooper@gmail.com>
2020-12-01 13:28:52 -0500

    updated strong params in food_access_point_controller since no real need for passing around a parameter

--- app/controllers/food_access_points_controller.rb
+++ app/controllers/food_access_points_controller.rb
@@ -16,7 +16,7 @@ class FoodAccessPointsController < ApplicationController
 
   def create
     Rails.logger.debug params.inspect
-    @food_access_point = current_user.food_access_points.build(food_access_point_create_params(params))
+    @food_access_point = current_user.food_access_points.build(food_access_point_create_params)
     if @food_access_point.save 
       redirect_to food_access_point_path(@food_access_point)
     else
@@ -30,7 +30,7 @@ class FoodAccessPointsController < ApplicationController
 
   def update
     @food_access_point = FoodAccessPoint.find(params[:id])
-    @food_access_point.update(food_access_point_edit_params(params))
+    @food_access_point.update(food_access_point_edit_params)
     if @food_access_point.save 
       puts "It saved?"
       redirect_to food_access_point_path(@food_access_point)
@@ -47,12 +47,12 @@ class FoodAccessPointsController < ApplicationController
 
   private 
 
-  def food_access_point_edit_params(fap_params) 
-    fap_params.require(:food_access_point).permit(:name, :address, :town, :state, :zip, :phone_number, :website)
+  def food_access_point_edit_params 
+    params.require(:food_access_point).permit(:name, :address, :town, :state, :zip, :phone_number, :website)
   end
 
-  def food_access_point_create_params(fap_params) 
-    fap_params.require(:food_access_point).permit(:name, :address, :town, :state, :zip, :phone_number, :website)
+  def food_access_point_create_params 
+    params.require(:food_access_point).permit(:name, :address, :town, :state, :zip, :phone_number, :website)
   end
 
 end
