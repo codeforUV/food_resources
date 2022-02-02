3b1a976bf4dd6f978234e56d8d271a7345ca52d2
Tom Chen <tom@builtbyargonaut.com>
2022-02-02 00:01:18 -0500

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,31 +5,43 @@
 #
 require 'csv'
 
-# Create a demo user
+user = User.find_or_initialize_by(email: "admin@example.com")
+user.update(password: "123456")
+user.save!
 
-user = User.create(email: "a@me.com", password: "123456")
-
-puts user.email + " created."
-
-# For Food Access Points, we use a csv file.  First step, read and parse the file.
 csv = CSV.parse(File.read(Rails.root.join('db', 'seeds.csv')), headers: true)
 
-# Create Food Access Points assigned to our user.
 csv.each do |row|
-  fap = user.food_access_points.build
-  fap.address = row["address"]
-  fap.town = row["town"]
-  fap.state = row["state"]
-  fap.zip = row["zip"]
-  fap.name = row["name"]
-  fap.phone_number = row["phone_number"]
-  fap.website = row["website"]
-  fap.website = "http://cnn.com" unless !fap.website.blank?
-  fap.save
-  puts fap.name + " saved." 
+  organization = Organization.find_or_create_by!(name: row["organization_name"])
+  location = Location.find_or_create_by(organization: organization, name: row["organization_name"])
+  service = Service.find_or_create_by!(name: row["service_name"], organization: organization, user: user)
+  service.update(description: row["service_description"], url: row["url"])
+  service_at_location = ServiceAtLocation.find_or_create_by!(location: location, service: service)
+  PhysicalAddress.find_or_create_by(
+    location: location,
+    address: row["address"],
+    city: row["city"],
+    state_province: row["state_province"],
+    postal_code: row["postal_code"],
+    country: "USA"
+  )
+  if row["freq"] 
+    Schedule.find_or_create_by!(
+      service: service,
+      location: location,
+      service_at_location: service_at_location,
+      freq: row["freq"],
+      interval: row["interval"],
+      byday: row["byday"],
+      opens_at: row["opens_at"],
+      closes_at: row["closes_at"],
+    )
+  end
+  Phone.find_or_create_by!(
+    location: location,
+    service: service,
+    organization: organization,
+    service_at_location: service_at_location,
+    number: row["phone"]
+  )
 end
- 
-
-
-
-# Create and save new food_access_points.
