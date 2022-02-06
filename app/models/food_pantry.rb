22919ef5e67a70ac0539846e9ee885479d88466b
Tom Chen <tom@builtbyargonaut.com>
2022-02-05 20:41:39 -0500

    use time_zone in parsed_schedules

--- app/models/food_pantry.rb
+++ app/models/food_pantry.rb
@@ -29,9 +29,10 @@ class FoodPantry < ApplicationRecord
     }) do |schedule, object|
       rrules = schedule["rrules"].first
       rule_type = rrules["rule_type"]
-      start_time = schedule["start_time"]
-      end_time = schedule["end_time"]
-      formatted_hours = "#{Time.parse(start_time).strftime("%-I:%M %p")} – #{Time.parse(end_time).strftime("%-I:%M %p")}"
+      start_time = schedule["start_time"]["time"]
+      end_time = schedule["end_time"]["time"]
+      time_zone = schedule["start_time"]["zone"]
+      formatted_hours = "#{Time.parse(start_time).in_time_zone(time_zone).strftime("%-I:%M %p")} – #{Time.parse(end_time).in_time_zone(time_zone).strftime("%-I:%M %p")}"
 
       if rule_type == "IceCube::WeeklyRule"
         days = rrules["validations"]["day"]
