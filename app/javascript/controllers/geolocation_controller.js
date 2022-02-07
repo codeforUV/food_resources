976cf84a164c4e80409cc4a32f90cd71fcb135fb
Tom Chen <tom@builtbyargonaut.com>
2022-02-06 23:08:59 -0500

    allow user to sort food_pantries by "near me"
    using the default geocoder distance of 20 miles for now

---
+++ app/javascript/controllers/geolocation_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  getCurrentPosition() {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        window.location.search = `?lat=${latitude}&lon=${longitude}}`;
+      });
+    } else {
+      window.alert("Sorry, geolocation is not supported by this browser");
+    }
+  }
+}
